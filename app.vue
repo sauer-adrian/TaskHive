d33bb500993007047aabc1556c91508645d05e84
Adrian Sauer <a.sauer@globos.de>
2025-02-13 16:05:18 +0100

    Added PWA support

--- app.vue
+++ app.vue
@@ -1,5 +1,6 @@
 <template>
   <UApp>
+    <VitePwaManifest />
     <NuxtLayout>
       <NuxtPage />
     </NuxtLayout>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,25 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxt/ui'],
+  modules: [
+    '@nuxt/ui',
+    '@vite-pwa/nuxt'
+
+  ],
+  pwa: {
+    manifest: {
+      name: "TaskHive",
+      short_name: "TaskHive",
+      description: "A modern, sleek, and intuitive to-do list web app built with Nuxt 3."
+    },
+    workbox: {
+      navigateFallback: '/',
+    },
+    devOptions:{
+      enabled: true,
+      type: "module",
+    }
+  },
   css: ['~/assets/css/main.css']
 })
+//TODO: Look into the manifest and configurate the PWA better, so it works!
